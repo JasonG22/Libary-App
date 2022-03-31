a8d1a92a315fe57c1685df2ed2a2c68ee563a2b4
JasonG22 <jase.gillett@gmail.com>
2022-03-31 09:11:46 +0100

    Updated script.js
    Added comments to assist with logic

--- js/script.js
+++ js/script.js
@@ -1,3 +1,38 @@
+/*
+Logic for code coming from dom
+create var for addBook Button = get element by ID book
+add eventListener for addBook click function
+{
+
+
+}
+
+const submitBook (button) = get element by ID submitBook
+add event listener for submitBook click function()
+{
+create vat for the following input fields
+const bookTitle = get elemembt by id title
+const bookAuthor = get elemnt by id author
+const bookStatus = get element by id status
+
+const book = object.create(Book);
+book.title = bookTitle
+book.author = bookAuthor
+book.status = bookStatus
+addBooktoLibary(book)
+}
+*/
+
+/*
+Fill out the object constructor params (title, author, status)
+this.title = title
+this.author = author
+this.status = status
+
+Add param to addBookToLibary (bookDetails)
+*/
+
+
 //Array to hold the books
 let myLibary = [];
 
@@ -8,5 +43,7 @@ function Book() {
 
 //Function to add the book to the page
 function addBookToLibary() {
-
+//add data attr to book
+//push book to array
 }
+
